7f27f0a07f606666b7b2d40696855df86b991405
Bruce Scherzinger <16126098+bascherz@users.noreply.github.com>
2018-04-22 16:57:42 -0400

    Version  1.2
    Added an option to add a number of days AFTER the event to the result.

--- src/helper.php
+++ src/helper.php
@@ -31,8 +31,9 @@ class ModNextEventHelper
         // Obtain a database connection
         $db = JFactory::getDbo();
 
-        // Retrieve the not-until date. This is actual a delta time and we have to compute when to start showing something.
-        $within = $params->get('within');
+        // Retrieve the not-until and dates.
+        $within = $params->get('within');   // This is actual a delta time and we have to compute when to start showing something.
+        $adddays = $params->get('adddays'); // This is a number of days to add to the returned date.
 
         // Get the actual date of the event
         $found = true;
@@ -51,7 +52,7 @@ class ModNextEventHelper
         }
 
         // Now see if we're within the specified interval
-        $query = "SELECT date_format(startrepeat,'$dateformat') as nextdate FROM #__jevents_vevdetail e JOIN #__jevents_repetition r ON e.evdet_id=r.eventdetail_id WHERE startrepeat > now() AND '$notifydate' <= now() AND summary='$eventname' ORDER BY startrepeat LIMIT 1";
+        $query = "SELECT date_format(DATE_ADD(startrepeat,INTERVAL '$adddays' DAY),'$dateformat') as nextdate FROM #__jevents_vevdetail e JOIN #__jevents_repetition r ON e.evdet_id=r.eventdetail_id WHERE startrepeat > now() AND '$notifydate' <= now() AND summary='$eventname' ORDER BY startrepeat LIMIT 1";
         $db->setQuery($query);
         $nextdate = $db->loadResult();
 
